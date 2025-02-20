14bdd35c2036265133ec5f894dfd1502cb97ba7d
Alex Schofield <git@ajschof.me>
2025-02-20 16:58:03 +0000

    add main logic to run.py

--- run.py
+++ run.py
@@ -2,7 +2,19 @@ import xml.etree.ElementTree as xml
 import sys
 
 def main(file):
-    pass
+    collection = xml.parse(file)
+    root = collection.getroot()
+
+    tracks = []
+
+    for track in root.iter("TRACK"):
+        tempos = track.findall("TEMPO")
+        if len(tempos) > 1:
+            name = track.get("Name")
+            tracks.append(name)
+
+    for name in tracks:
+        print(name)
 
 if __name__ == "__main__":
     main(str(sys.argv[1]))
